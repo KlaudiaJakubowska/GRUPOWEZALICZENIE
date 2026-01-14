2ee7d89445b42ade270b7e8775f6837570726603
nataliazme <149538445+nataliazme@users.noreply.github.com>
2026-01-14 16:09:20 +0100

    Działamy !!!
    Czy ja robię coś źle ? (drobne problemy techniczne ale na ścianie działa)

---
+++ pathways/Assets/_Unity Essentials/Scripts/Triggertext.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+
+public class Triggertext : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(false);
+        }
+
+    }
+}
